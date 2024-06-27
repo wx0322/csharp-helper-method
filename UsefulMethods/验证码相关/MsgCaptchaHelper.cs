198cf683892800fa4c9b17cf1c71a82b5dc94c72
wx0322 <wx_0322@outlook.com>
2024-06-27 16:36:13 +0800

    update

---
+++ UsefulMethods/验证码相关/MsgCaptchaHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace 验证码相关 {
+
+  /// <summary>
+  /// 短信验证码帮助类
+  /// </summary>
+  public class MsgCaptchaHelper {
+
+    /// <summary>
+    /// 生成指定位数的随机数字码
+    /// </summary>
+    /// <param name="aLength"></param>
+    /// <returns></returns>
+    public static string CreateRandomNumber(int aLength) {
+      Random bRandom = new Random();
+      StringBuilder bSBMsgCode = new StringBuilder();
+      for (int i = 0; i < aLength; i++) {
+        bSBMsgCode.Append(bRandom.Next(0, 9));
+      }
+
+      return bSBMsgCode.ToString();
+    }
+  }
+}
